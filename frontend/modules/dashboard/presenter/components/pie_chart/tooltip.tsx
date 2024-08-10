568cc0849724c60bf2084a05b84f607f221ab9a6
MiguelG <miguel.gutierrezt@pucp.pe>
2024-08-10 11:08:51 -0500

    tooltip value

--- frontend/modules/dashboard/presenter/components/pie_chart/tooltip.tsx
+++ frontend/modules/dashboard/presenter/components/pie_chart/tooltip.tsx
@@ -25,11 +25,11 @@ export default function Render_Tooltip(
       <p className="text-[1.4rem]">{payload[0].name}</p>
 
       <div className="text-[1.4rem] flex gap-2">
-        <p>{`${(
+        <p>{`${payload[0].value}`}</p>
+        <p>{`(${(
           ((payload[0].value as number) * 100) /
           activeToolTipAccumValue
-        ).toFixed(1)}%`}</p>
-        <p>{`(${payload[0].value})`}</p>
+        ).toFixed(1)}%)`}</p>
       </div>
     </div>
   );
