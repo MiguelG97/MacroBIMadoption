21a8c9caba27231da8cb69447125595977b1ebc2
MiguelG <miguel.gutierrezt@pucp.pe>
2024-10-19 15:57:06 -0500

    dummy image

--- frontend/modules/dashboard/presenter/screens/tabViews/7_non_adopters.tsx
+++ frontend/modules/dashboard/presenter/screens/tabViews/7_non_adopters.tsx
@@ -4,6 +4,7 @@ import { IQuestionnaire } from "@/core/shared/types/postgresql_schema_types";
 import React, { useMemo } from "react";
 import Pie_chart_bim from "../../components/pie_chart";
 import Bar_chart_bim from "../../components/bar_chart";
+import Image from "next/image";
 
 export default function NonAdopters_tabView() {
   /**Redux toolkit */
@@ -25,7 +26,18 @@ export default function NonAdopters_tabView() {
         <Pie_chart_bim questionnaire={questionnairesNonAdopters[1]} />
       </div>
       <div className="flex flex-row gap-[2.4rem] w-full justify-center">
-        <Bar_chart_bim fullWidth questionnaire={questionnairesNonAdopters[2]} />
+        <Bar_chart_bim questionnaire={questionnairesNonAdopters[2]} />
+        <div
+          className="relative max-w-[70rem] w-full flex justify-center items-center
+        pt-[2.4rem] px-[2.4rem] pb-[0.8rem]"
+        >
+          <Image
+            src={"/assets/images/construction_plan2d.jpg"}
+            alt="plan2d"
+            fill
+            className="rounded-[1.6rem]"
+          />
+        </div>
       </div>
     </div>
   );
